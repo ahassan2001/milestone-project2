c37f5202dc90f504e7c040e87ce7329c85f5dd89
unknown <ahassan2001@msn.com>
2024-03-23 14:51:20 -0400

    added Users.js

---
+++ frontend/frontend/src/components/Users.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+    const [users, setUsers] = useState([
+        { username: '', password: '', _id: '1' },
+    ]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/users')
+            .then(res => setUsers(res.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            <h2>Users</h2>
+            {users.map(user => (
+                <div key={user._id}>
+                    <h3>{user.username}</h3>
+                    <p>{user.password}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Users;
