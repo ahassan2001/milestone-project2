b906648b3464632e041c49541e015974e1ddc21e
unknown <ahassan2001@msn.com>
2024-03-23 14:49:15 -0400

    added SignIn.js

---
+++ frontend/frontend/src/components/SignIn.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../styles.css';
+
+function SignIn(props) {
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/users', user)
+            .then(res => {
+                console.log(res.data);
+                props.onSignIn();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input
+                    type="text"
+                    name="username"
+                    value={user.username}
+                    onChange={handleChange}
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    value={user.password}
+                    onChange={handleChange}
+                />
+                <br/>
+                <br/>
+                <button className="sign-in" type="submit">Sign In</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignIn;
