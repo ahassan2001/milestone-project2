4820f2646c7ea34ab9cebd840c74e676cebc85f6
unknown <ahassan2001@msn.com>
2024-03-23 14:52:19 -0400

    added App.js

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles.css'
+import TaskCard from './components/TaskCard';
+import Users from './components/Users';
+import SignIn from './components/SignIn';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState({
+    title: '',
+    description: '',
+    priority: 1,
+    category: '',
+    dueDate: '',
+  });
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/tasks')
+    .then(res => setTasks(res.data))
+    .catch(err => console.log(err));
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewTask(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleNewSubmit = (e) => {
+    e.preventDefault();
+      axios.post('http://localhost:5000/api/tasks', newTask)
+        .then(res => {
+          setTasks([...tasks, res.data]);
+          setNewTask({
+            title: '',
+            description: '',
+            priority: 1,
+            category: '',
+            dueDate: '',
+          });
+        })
+        .catch(err => console.log(err));
+  };
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+      axios.put(`http://localhost:5000/api/tasks/${newTask._id}`, newTask)
+        .then(res => {
+          const updatedTasks = tasks.map(task =>
+            task._id === newTask._id ? res.data : task
+          );
+          setTasks(updatedTasks);
+          setNewTask({
+            title: '',
+            description: '',
+            priority: 1,
+            category: '',
+            dueDate: '',
+          });
+        })
+        .catch(err => console.log(err));
+      }
+
+  const editTask = (task) => {
+    setNewTask(task);
+  };
+
+  const deleteTask = (taskId) => {
+    axios.delete(`http://localhost:5000/api/tasks/${taskId}`)
+      .then(() => {
+        const updatedTasks = tasks.filter(task => task._id !== taskId);
+        setTasks(updatedTasks);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const handleSignIn = () => {
+    setIsSignedIn(true);
+    setUsername(username);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {!isSignedIn && <SignIn onSignIn={handleSignIn} />}
+      {isSignedIn && (
+        <>
+          <h1>Task Manager</h1>
+          <form onSubmit={handleNewSubmit}>
+            <label>Title:</label>
+            <input type="text" name="title" value={newTask.title} onChange={handleChange} required />
+            <label>Description:</label>
+            <textarea name="description" value={newTask.description} onChange={handleChange} required />
+            <label>Priority:</label>
+            <input type="number" name="priority" value={newTask.priority} onChange={handleChange} required/>
+            <br/>
+            <br/>
+            <label>Category:</label>
+            <input type="text" name="category" value={newTask.category} onChange={handleChange} />
+            <label>Due Date:</label>
+            <input type="date" name="dueDate" value={newTask.dueDate} onChange={handleChange} />
+            <br/>
+            <br/>
+            <button type="submit" className="add-button">Add Task</button>
+          </form>
+          {tasks.map((task, index) => ( 
+            <TaskCard key={index} task={task} handleDelete={deleteTask} handleEdit={handleEditSubmit} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
 
-export default App;
+export default App; 
