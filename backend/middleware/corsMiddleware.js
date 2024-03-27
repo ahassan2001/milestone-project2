6a24688429600c550689b8c8bb823275195f2c19
unknown <ahassan2001@msn.com>
2024-03-27 00:49:49 -0400

    cors middleware

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const corsMiddleware = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
 
-const app = express();
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
 
-app.use((req, res, next) => {
-res.setHeader('Access-Control-Allow-Origin', 'https://milestone-project2-seven.vercel.app'); // Replace with your frontend URL
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-next();
-});
+module.exports = corsMiddleware(handler);
